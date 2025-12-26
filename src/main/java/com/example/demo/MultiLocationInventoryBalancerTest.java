a8fa342bd4073cdee5933f3b32c2ef2fb6045fa4
bala-amypo <bala@amypo.in>
2025-12-26 06:18:39 +0000

    User commit

---
+++ src/main/java/com/example/demo/MultiLocationInventoryBalancerTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiLocationInventoryBalancerTest {
+
+    @Test
+    void contextLoads() {
+        // Test passes if Spring Boot application starts
+    }
+}
